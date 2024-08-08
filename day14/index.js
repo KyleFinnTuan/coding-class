d92e1df7c3596a0b3d88b1170bb48598f4b17ce8
KyleFinnTuan <wandinainggolan9@gmail.com>
2024-08-08 18:32:00 +0800

    new

---
+++ day14/index.js
@@ -0,0 +1,52 @@
+// const food = 'jus alpukat';
+
+// switch (food) {
+//     case 'rendang':
+//         console.log('ini rendang');
+//         break;
+
+//         case 'nasgor':
+//         console.log('ini nasi goreng');
+//         break;
+
+//         case 'jus jeruk':
+//         case 'jus alpukat':
+//         console.log('ini jus');
+//         break;
+
+//     default:
+//         console.log('Gw ngk ketemu yang ini');
+//         break;
+// }
+
+// switch (true) {
+//     case 1+1 === 2:
+//         console.log('1+1 sama dengan 2');
+//         break;
+
+//     case 1 > 2:
+//         console.log('1 lebih besar kecil dari 2');
+//         break;
+
+//     default:
+//         console.log('Tidak ada yang benar');
+//         break;
+// }
+
+const pet = 'dragon';
+
+switch (pet) {
+    case 'cat':
+    case 'dog':
+        console.log('I am an animal');
+        break;
+    case 'sunFlower':
+        console.log('I am a Plant');
+        break;
+    case 'human':
+        console.log('I am only human afterall');
+        break;
+    default:
+        console.log('I am an alien');
+        break;
+}
